33efd16ede95386c2a7500fd53d625d4f6f9aa22
Churikov0112 <churikov0112@github.com>
2026-07-15 18:18:07 +0300

    feat(penalty): rebuild glb with penalty_kick_l/_r run-up clips

--- tests/check_footballer_glb.gd
+++ tests/check_footballer_glb.gd
@@ -17,6 +17,7 @@ func _initialize() -> void:
 		var anims := ap.get_animation_list()
 		print("CHECK: анимации в glb = ", anims)
 		for want in ["idle", "run", "roll_left", "roll_right",
+				"penalty_kick_l", "penalty_kick_r",
 				"keeper_idle", "keeper_sidestep", "keeper_body_block_l", "keeper_body_block_r",
 				"keeper_diving_save_l", "keeper_diving_save_r", "keeper_catch", "keeper_catch_top",
 				"keeper_catch_head",
